98d755d66089244cb4672ebf84ae9e46f44f6ce7
mridubhatnagar <mridubhatnagar@rediffmail.com>
2020-01-25 21:51:14 +0530

    models condition changed

--- bankdetails/models.py
+++ bankdetails/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Bank(models.Model):
 	ifsc=models.CharField(max_length=11, null=False, blank=False, primary_key=True)
 	bank_id=models.BigIntegerField()
-	branch=models.CharField(max_length=74, null=False, blank=False)
-	address=models.CharField(max_length=195, null=False, blank=False)
-	city=models.CharField(max_length=50, null=False, blank=False)
-	district=models.CharField(max_length=50, null=False, blank=False)
-	state=models.CharField(max_length=50, null=False, blank=False)
-	bank_name=models.CharField(max_length=50, null=False, blank=False)
+	branch=models.CharField(max_length=74)
+	address=models.CharField(max_length=195)
+	city=models.CharField(max_length=50)
+	district=models.CharField(max_length=50)
+	state=models.CharField(max_length=50)
+	bank_name=models.CharField(max_length=50)
 
 
 	class Meta:
