921dd0e751f47141d6e94ef7e3ad076934e75b5b
mridubhatnagar <mridubhatnagar@rediffmail.com>
2020-01-25 14:58:49 +0530

    path change

--- banks/urls.py
+++ banks/urls.py
@@ -19,7 +19,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
-    path('', include('bankdetails.urls')),
+    path('^$', include('bankdetails.urls')),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view())
     
