41d23a7537a3a1934ce43cd00b520c448744da50
mridubhatnagar <mridubhatnagar@rediffmail.com>
2020-01-25 14:37:39 +0530

    secret key and allowed hosts updated

--- banks/settings.py
+++ banks/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import datetime
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,12 +22,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v56ou(dmd(dqp@ij6_sr$0ej4juep^*7l@(h8*l+is$%uusf7o'
+SECRET_KEY = '187944063f4e11ea989cd46e0e0c2a4a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "https://branchdetailsapp.herokuapp.com/"]
+
+django_heroku.settings(locals())
 
 
 # Application definition
