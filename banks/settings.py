93f1a88fae0266b023f14b61402318913b092140
mridubhatnagar <mridubhatnagar@rediffmail.com>
2020-01-25 15:04:38 +0530

    allowed hosts changed

--- banks/settings.py
+++ banks/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '187944063f4e11ea989cd46e0e0c2a4a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*", "https://branchdetailsapp.herokuapp.com/"]
+ALLOWED_HOSTS = ["https://branchdetailsapp.herokuapp.com/"]
 
 
 
