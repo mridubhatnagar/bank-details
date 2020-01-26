a4c82a1a0503a19ca97e72e6dba0bd1028346bbe
mridubhatnagar <mridubhatnagar@rediffmail.com>
2020-01-26 11:27:53 +0530

    simple JWT settings

--- banks/settings.py
+++ banks/settings.py
@@ -143,39 +143,20 @@ REST_FRAMEWORK = {
     ],
 }
 
-JWT_AUTH = {
-    'JWT_ENCODE_HANDLER':
-    'rest_framework_jwt.utils.jwt_encode_handler',
 
-    'JWT_DECODE_HANDLER':
-    'rest_framework_jwt.utils.jwt_decode_handler',
 
-    'JWT_PAYLOAD_HANDLER':
-    'rest_framework_jwt.utils.jwt_payload_handler',
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPE': 'Bearer',
 
-    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
-    'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+    'USER_ID_FIELD': 'id',
+    'PAYLOAD_ID_FIELD': 'user_id',
 
-    'JWT_RESPONSE_PAYLOAD_HANDLER':
-    'rest_framework_jwt.utils.jwt_response_payload_handler',
+    'TOKEN_LIFETIME': datetime.timedelta(days=5),
+    'TOKEN_REFRESH_LIFETIME': datetime.timedelta(days=5),
 
-    'JWT_GET_USER_SECRET_KEY': None,
-    'JWT_PUBLIC_KEY': None,
-    'JWT_PRIVATE_KEY': None,
-    'JWT_ALGORITHM': 'HS256',
-    'JWT_VERIFY': True,
-    'JWT_VERIFY_EXPIRATION': True,
-    'JWT_LEEWAY': 0,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300),
-    'JWT_AUDIENCE': None,
-    'JWT_ISSUER': None,
+    'SECRET_KEY': SECRET_KEY,  # Default to the django secret key
 
-    'JWT_ALLOW_REFRESH': False,
-    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=5),
-
-    'JWT_AUTH_HEADER_PREFIX': 'JWT',
-    'JWT_AUTH_COOKIE': None,
-
+    'TOKEN_BACKEND': 'rest_framework_simplejwt.backends.TokenBackend',
 }
 
 django_heroku.settings(locals())
