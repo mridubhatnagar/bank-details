ba58ceddf8c563680d23721ef76751d3e921b0c4
mridubhatnagar <mridubhatnagar@rediffmail.com>
2020-01-27 22:07:20 +0530

    debug true

--- banks/settings.py
+++ banks/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '187944063f4e11ea989cd46e0e0c2a4a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["https://branchdetailsapp.herokuapp.com/"]
 
