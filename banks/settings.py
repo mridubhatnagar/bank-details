071f75a780de5967bb36154b572123b9c4186b1d
mridubhatnagar <mridubhatnagar@rediffmail.com>
2020-02-04 21:10:49 +0530

    variables are fetched from environment

--- banks/settings.py
+++ banks/settings.py
@@ -10,9 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+from dotenv import load_dotenv
 import os
 import datetime
 import django_heroku
+
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,12 +84,16 @@ WSGI_APPLICATION = 'banks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+print(os.environ.get("DB_NAME"))
+print(os.environ.get("DB_USER"))
+print(os.environ.get("DB_PASSWORD"))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bankinformation',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': 'localhost',
         'PORT': '',
     }
